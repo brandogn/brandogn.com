7b2f47ea4ef727f16146e56666813cf07823e6d4
Brandon <bwong928@berkeley.edu>
2022-12-16 18:58:30 -0800

    website up

---
+++ main.js
@@ -0,0 +1,30 @@
+const tabs = document.querySelectorAll("nav button");
+let currTitle = tabs[0].dataset.title;
+
+function focus(title) {
+  const page = document.querySelectorAll(`[data-title="${title}"]`);
+  page.forEach((e) => {
+    e.dataset.status = "active";
+  });
+}
+
+function unFocus(title) {
+  const page = document.querySelectorAll(`[data-title="${title}"]`);
+  page.forEach((e) => {
+    e.dataset.status = "inactive";
+  });
+}
+
+function clickHandler(elem) {
+  return () => {
+    unFocus(currTitle);
+    currTitle = elem.dataset.title;
+    focus(currTitle);
+  };
+}
+
+for (const tab of tabs) {
+  tab.onclick = clickHandler(tab);
+}
+
+focus(currTitle);
